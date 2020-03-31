678a9b9828209e2922e8d16c250916ebb6a4142b
AashiqinCode <aashiqahmed10101@gmail.com>
2020-03-31 23:01:53 +0530

    initial commit-Grid setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'fds-grid';
+//Component is decorator(A design patter),i.e they are all functions/methods with some meta data holdings
+export class AppComponent  {
+
+  constructor(private http: HttpClient){
+
+  }
+  title="FDS-GRID";
+  
+  rowData: any=[];
+    
+    columnDefs = [
+        {headerName: 'Name', field: 'name', sortable: true },
+        {headerName: 'ID', field: 'id', sortable: true },
+        {headerName: 'Department', field: 'dept', sortable: true },
+
+      ];
+
+        // { make: 'Toyota', model: 'Celica', price: 35000 },
+        // { make: 'Ford', model: 'Mondeo', price: 32000 },
+        // { make: 'Porsche', model: 'Boxter', price: 72000 }
+        ngOnInit() {
+          this.rowData = this.http.get('https://api.myjson.com/bins/668dg');
+      }
+    
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule.withComponents([]),
+    // /The withComponents call is necessary for the grid to be able to use Angular components as cells / headers - you can ignore it for now.
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
