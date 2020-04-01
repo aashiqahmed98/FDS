f6d7cbfe41c86462ef0a84213d8eee02f2738334
AashiqinCode <aashiqahmed10101@gmail.com>
2020-04-01 15:07:46 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,30 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  styleUrls: ['./app.component.scss']
 })
 //Component is decorator(A design patter),i.e they are all functions/methods with some meta data holdings
-export class AppComponent  {
+export class AppComponent {
+  constructor(private http: HttpClient) {}
+  title = 'FDS-GRID';
 
-  constructor(private http: HttpClient){
+  rowData: any = [];
 
+  columnDefs = [
+    { headerName: 'Name', field: 'name', sortable: true },
+    // checkboxSelection: true
+    { headerName: 'ID', field: 'id', sortable: true },
+    { headerName: 'Department', field: 'dept', sortable: true }
+  ];
+
+  // { make: 'Toyota', model: 'Celica', price: 35000 },
+  // { make: 'Ford', model: 'Mondeo', price: 32000 },
+  // { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ngOnInit() {
+    this.rowData = this.http.get('https://api.myjson.com/bins/668dg');
+    // Used my own api created using dummy json code->http://myjson.com/
   }
-  title="FDS-GRID";
-  
-  rowData: any=[];
-    
-    columnDefs = [
-        {headerName: 'Name', field: 'name', sortable: true },
-        {headerName: 'ID', field: 'id', sortable: true },
-        {headerName: 'Department', field: 'dept', sortable: true },
-
-      ];
-
-        // { make: 'Toyota', model: 'Celica', price: 35000 },
-        // { make: 'Ford', model: 'Mondeo', price: 32000 },
-        // { make: 'Porsche', model: 'Boxter', price: 72000 }
-        ngOnInit() {
-          this.rowData = this.http.get('https://api.myjson.com/bins/668dg');
-      }
-    
-
 }
